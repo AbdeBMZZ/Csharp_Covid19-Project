4a8059cb2f13b591001cf5f321a91f3c7ce9d42a
Abdellah Boumaiza <boumaizaabdellah01@gmail.com>
2020-12-15 12:33:26 +0100

    change

---
+++ covidPatient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace covidPatient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("abdo");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ covidPatient/citoyen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace covidPatient
+{
+    class citoyen
+    {
+        private int citoyenID;
+        private string firstName;
+        private string lastName;
+        private string address;
+        private int age;
+        private bool contactwithcase;
+        private bool covid;
+
+        public void tester() { }
+
+    }
+}
