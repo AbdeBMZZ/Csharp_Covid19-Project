157a1f2934393c023172c5dffbac664d14f62c51
Abdellah Boumaiza <abdellah.boumaiza@usmba.ac.ma>
2021-01-03 16:26:19 +0100

    not done yet

--- Covid 19 WinForm/suivi les patients.Designer.cs
+++ Covid 19 WinForm/suivi les patients.Designer.cs
@@ -28,18 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.en_quarantaine = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // en_quarantaine
             // 
-            this.en_quarantaine.Location = new System.Drawing.Point(80, 100);
+            this.en_quarantaine.Location = new System.Drawing.Point(188, 107);
+            this.en_quarantaine.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.en_quarantaine.Name = "en_quarantaine";
-            this.en_quarantaine.Size = new System.Drawing.Size(131, 52);
+            this.en_quarantaine.Size = new System.Drawing.Size(153, 56);
             this.en_quarantaine.TabIndex = 0;
             this.en_quarantaine.Text = "en quarantaine";
             this.en_quarantaine.UseVisualStyleBackColor = true;
@@ -47,42 +50,60 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(250, 100);
+            this.button2.Location = new System.Drawing.Point(386, 107);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 50);
+            this.button2.Size = new System.Drawing.Size(153, 54);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "en reanimation";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(420, 100);
+            this.button3.Location = new System.Drawing.Point(584, 107);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(131, 55);
+            this.button3.Size = new System.Drawing.Size(153, 60);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(80, 183);
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.Location = new System.Drawing.Point(93, 200);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(444, 206);
+            this.dataGridView1.Size = new System.Drawing.Size(750, 222);
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.Visible = false;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            this.dataGridView1.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_DefaultValuesNeeded);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // suivi_les_patients
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(933, 485);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.en_quarantaine);
+            this.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "suivi_les_patients";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "suivi_les_patients";
+            this.Load += new System.EventHandler(this.suivi_les_patients_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -94,5 +115,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Covid 19 WinForm/suivi les patients.cs
+++ Covid 19 WinForm/suivi les patients.cs
@@ -14,32 +14,91 @@ namespace Covid_19_WinForm
     public partial class suivi_les_patients : Form
     {
         static string chaine = @"Data Source=localhost;Initial Catalog=Covid winForm;Integrated Security=True";
+
         public suivi_les_patients()
         {
-
             InitializeComponent();
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            btn.HeaderText = "Refaire PCR";
+            btn.Name = "pcr";
+            btn.Text = "refaire pcr";
+            btn.UseColumnTextForButtonValue = true;
+            dataGridView1.Columns.Add(btn);
+
+            DataGridViewTextBoxColumn textboxColumn = new DataGridViewTextBoxColumn();
+            textboxColumn.Name = "days";
+            textboxColumn.HeaderText = "jrs en quarantaine";
+            
+            dataGridView1.Columns.Add(textboxColumn);
+
+
         }
+
 
         private void en_quarantaine_Click(object sender, EventArgs e)
         {
 
             SqlConnection con = new SqlConnection(chaine);
-            SqlCommand cmd = new SqlCommand("SELECT citoyen.citoyen_cin, citoyen.citoyen_name, citoyen.citoyen_address, test.test_res FROM citoyen,test WHERE  citoyen.citoyen_cin=test.citoyen_cin and test.test_res = @res", con);
+            SqlCommand cmd = new SqlCommand("SELECT citoyen.citoyen_cin as CIN, citoyen.citoyen_name as Name, citoyen.citoyen_address as Address, test.test_res as PCR FROM citoyen,test WHERE  citoyen.citoyen_cin=test.citoyen_cin and test.test_res = @res", con);
             con.Open();
             cmd.Parameters.AddWithValue("@res", "POSITIF");
 
-            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
+            var dt = new DataTable();
+
+            using (var sqlDataReader = cmd.ExecuteReader())
+            {
+                dt.Load(sqlDataReader);
+            }
+
             dataGridView1.DataSource = dt;
 
             dataGridView1.Visible = true;
 
             con.Close();
+            
+
 
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+
+                e.CellStyle.BackColor = Color.Orange;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["pcr"].Index)
+            {
+                int rowIndx = e.RowIndex;
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                MessageBox.Show(row.Cells["Name"].Value.ToString());
+
+            }
+        }
+
+        private void suivi_les_patients_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            e.Row.Cells["days"].Value = "1";
+
+        }
     }
-    }   
+}
+
 
 
